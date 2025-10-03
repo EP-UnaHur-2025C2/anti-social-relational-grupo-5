7f683fd4f33a16b16ac1fa3caeeb8a7619856435
Ger-B <germanignacio.bianco@estudiantes.unahur.edu.ar>
2025-10-03 18:06:14 -0300

    Agrega dependencias, y la estructura de carpetas.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const app = express
+
+// const db = require('')
+
+app.use(express.json)
+
+const PORT = 3000
+
+app.listen(PORT, async()=>{
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`)
+    await db.sequelize.sync()
+})
+
 console.log("UnaHur - Anti-Social net");
