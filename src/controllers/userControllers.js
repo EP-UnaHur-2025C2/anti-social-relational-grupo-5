68fea1036b6c6119ab4588f5e954ade81d58c8e2
Ger-B <germanignacio.bianco@estudiantes.unahur.edu.ar>
2025-10-13 12:29:24 -0300

    Agrega las rutas y controladores para modificar y eliminar usuarios

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,46 +1,88 @@
-const {User} = require('../../db/models')
+const { where } = require('sequelize')
+const { User } = require('../../db/models')
 
-//onst db = require('../../db/models')
-//const serie = db.serie
 
-const obtenerUsuarios = async(req,res)=>{
-    try{
+const obtenerUsuarios = async (req, res) => {
+    try {
         const usuarios = await User.findAll()
         res.status(200).json(usuarios)
-    }catch{
-        res.status(500).json({mensaje: 'Error al obtener usuarios'})
+    } catch {
+        res.status(500).json({ mensaje: 'Error al obtener usuarios' })
     }
 }
 
-const obtenerUsuario = async(req,res)=>{
-    try{
-        const id = req.params.id    
-        const usuario = await User.findByPk(id)
-        if(!usuario){
-            res.status(400).json({mensaje: 'Usuario no encontrado'})
+const obtenerUsuario = async (req, res) => {
+    try {
+        const idNickName = req.params.idNickName
+        const usuario = await User.findByPk(idNickName)
+        if (!usuario) {
+            res.status(400).json({ mensaje: 'Usuario no encontrado' })
         }
         res.status(200).json(usuario)
-    }catch{
-        res.status(500).json({mensaje: 'Error al obtener usuario'})
+    } catch {
+        res.status(500).json({ mensaje: 'Error al obtener usuario' })
     }
 }
 
-const crearUsuario = async(req,res)=>{
-    try{
-        const {nickName, nombre, apellido}= req.body 
+const crearUsuario = async (req, res) => {
+    try {
+        const { nickName, nombre, apellido } = req.body
         const usuario = await User.create({
             nickName,
             nombre,
             apellido
         })
         res.status(201).json(usuario)
-    }catch{
-        res.status(500).json({mensaje: 'Error al obtener usuario'})
+    } catch {
+        res.status(500).json({ mensaje: 'Error al obtener usuario' })
+    }
+}
+
+const actualizarUsuario = async (req, res) => {
+    try {
+        const { idNickName } = req.params;
+        const { nickName, nombre, apellido } = req.body;
+        const usuario = await User.findByPk(idNickName);
+        if (!usuario) return res.status(404).json({ error: 'Usuario no encontrado' });
+        await User.update(
+            { 
+                nickName, 
+                nombre, 
+                apellido 
+            },
+            {
+                where: {
+                    nickName: usuario.nickName
+                }
+            }
+            );
+        res.status(200).json(usuario);
+    } catch (error) {
+        res.status(500).json({ message: "Error al sctualizar el usuario" })
+    }
+}
+
+const eliminarUsuario = async (req, res) => {
+    try {
+        const { idNickName } = req.params;
+        const usuario = await User.findByPk(idNickName);
+        if (!usuario) return res.status(404).json({ error: 'Usuario no encontrado' });
+        await User.destroy({
+            where: {
+                nickName: usuario.nickName
+            }
+        });
+        res.status(204).send();
+    } catch (error) {
+
+        res.status(500).json({ message: "Error al eliminar el usuario", error: {error} })
     }
 }
 
 module.exports = {
     obtenerUsuario,
     obtenerUsuarios,
-    crearUsuario
+    crearUsuario,
+    actualizarUsuario,
+    eliminarUsuario
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,9 +4,9 @@ const router = Router();
 const userController = require('../controllers/userControllers');
 
 router.get('/', userController.obtenerUsuarios );
-router.get('/:nickName', userController.obtenerUsuario);
-router.post('/', userController.crearUsuario);
-// router.put('/', );
-// router.delete('/', );
-
+router.get('/:idNickName', userController.obtenerUsuario);
+router.post('/', userController.crearUsuario); //Validar los campos para crear el usuairo(nickName sobre todo)
+router.put('/:idNickName', userController.actualizarUsuario);
+router.delete('/:idNickName', userController.eliminarUsuario);
+//Validar los idNickName
 module.exports = router
